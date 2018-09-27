29f330c9f591f9f15b09b570d016c1bb0776c364
vagolli <v.agolli@live.com>
2018-09-27 12:13:26 +0200

    init express server, nodemon

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-// TODO: create a basic server with express
-// that will send back the index.html file on a GET request to '/'
-// it should then send back jsonData on a GET to /data
+const express = require('express');
+const app = express();
 
-var jsonData = {count: 12, message: 'hey'};
+var jsonDAta = { count: 12, message: "hey" };
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html", function (err) {
+        if (err) {
+            res.status(500).send(err);
+        }
+    })
+});
+
+app.get("/data", function (req, res) {
+    res.json(jsonDAta);
+});
+
+app.listen(3000, () => {
+    console.log('HTTP server listening on port 3000');
+});
