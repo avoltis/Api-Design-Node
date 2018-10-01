a7eeca56e10b274331e5ccd9992d0357a9e2d202
vagolli <v.agolli@live.com>
2018-10-01 15:32:42 +0200

    Authentication Configuration

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // intro point for our server.
-// PRO-TIP: if you have an index.js file
+// if you have an index.js file
 // on the root of a folder in node
 // you can just require that folder and node will
 // automatically require the index.js on the root
